37ddb224c25f481ea2dcb32c129e96f677f8759c
artikosamodra <artikosamudra@gmail.com>
2024-01-24 21:16:07 +0700

    fixed footer setting

--- src/script.js
+++ src/script.js
@@ -3,7 +3,10 @@ function toggleBtn(articleId, hiddenId, showId) {
   let hiddenA = document.getElementById(hiddenId);
   let showA = document.getElementById(showId);
 
-  if (fullArticle.style.display === "none" || fullArticle.style.display === "") {
+  if (
+    fullArticle.style.display === "none" ||
+    fullArticle.style.display === ""
+  ) {
     fullArticle.style.display = "block";
     showA.style.display = "none";
     hiddenA.style.display = "block";
@@ -13,3 +16,27 @@ function toggleBtn(articleId, hiddenId, showId) {
     hiddenA.style.display = "none";
   }
 }
+
+// const NavbarNav = document.querySelector(".navbar-nav");
+
+// document.querySelector(
+//   ("#menu".onclick = (e) => {
+//     NavbarNav.classList.toggle(".active");
+//     e.preventDefault();
+//   })
+// );
+
+// function menuBar() {
+//     let menuButton = document.getElementById('menu')
+//     let navbarNav = document.getElementById('navbar')
+// }
+
+const menuButton = document.getElementById("menu");
+const navbarNav = document.getElementById("navbar");
+
+document.querySelector(
+  (menuButton.onclick = (e) => {
+    navbarNav.classList.toggle("active");
+    e.preventDefault();
+  })
+);
